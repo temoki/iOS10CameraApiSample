51ab4a2294fd3e2b7a05f288fa2c499fdc13cb0e
temoki <temoki@gmail.com>
2016-09-18 20:58:20 +0900

    プロジェクト作成

---
+++ iOS10CameraApiSample/iOS10CameraApiSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS10CameraApiSample
+//
+//  Created by temoki on 2016/09/18.
+//  Copyright © 2016年 temoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
