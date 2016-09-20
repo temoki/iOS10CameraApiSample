60fcc7c24f61f1108c2dcccde9180c08cef9a905
temoki <temoki@gmail.com>
2016-09-21 00:19:18 +0900

    update

--- iOS10CameraApiSample/iOS10CameraApiSample/ViewController.swift
+++ iOS10CameraApiSample/iOS10CameraApiSample/ViewController.swift
@@ -17,12 +17,14 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     @IBOutlet weak var capturePhotoButton: UIButton!
     
     private var captureSession: AVCaptureSession?
-    private let captureOutput = AVCapturePhotoOutput()
     
-    private var selectedPixelFormat: PixelFormat? {
+    private var imageFormatType: OSType? {
         didSet {
-            guard let format = selectedPixelFormat else { return }
-            pixelFormatLabel.text = format.description
+            if let type = imageFormatType {
+                pixelFormatLabel.text = "RAW (\(type))"
+            } else {
+                pixelFormatLabel.text = "JPEG"
+            }
         }
     }
     
@@ -62,7 +64,7 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
     
     @IBAction func selectPixelFormatButtonAction(sender: UIButton) {
-        selectPixelFormat()
+        selectPixelFormatType()
     }
     
     @IBAction func capturePhotoButtonAction(sender: UIButton) {
@@ -104,41 +106,30 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         present(actionSheet, animated: true, completion: nil)
     }
     
-    private var availablePixelFormats: [PixelFormat] {
-        var availablePixelFormats: [PixelFormat] = []
-        
-        for typeNumber in captureOutput.availablePhotoPixelFormatTypes {
-            var copiedPixelFormat = PixelFormat.pixelFormat(ofType: typeNumber.uint32Value)
-            copiedPixelFormat.isRaw = false
-            availablePixelFormats.append(copiedPixelFormat)
+    private func selectPixelFormatType() {
+        let actionSheet = UIAlertController(title: "Select Image Format", message: nil, preferredStyle: .actionSheet)
+        if let captureOutput = captureSession?.outputs?.first as? AVCapturePhotoOutput {
+            for rawPixelFormat in captureOutput.availableRawPhotoPixelFormatTypes {
+                actionSheet.addAction(UIAlertAction(title: "RAW (\(rawPixelFormat))", style: .default, handler: { [weak self] (action) in
+                    self?.imageFormatType = rawPixelFormat.uint32Value
+                    }))
+            }
         }
-        
-        for typeNumber in captureOutput.availableRawPhotoPixelFormatTypes {
-            var copiedPixelFormat = PixelFormat.pixelFormat(ofType: typeNumber.uint32Value)
-            copiedPixelFormat.isRaw = false
-            availablePixelFormats.append(copiedPixelFormat)
-        }
-        
-        return availablePixelFormats
-    }
-    
-    private func selectPixelFormat() {
-        let actionSheet = UIAlertController(title: "Select Pixel Format", message: nil, preferredStyle: .actionSheet)
-        for pixelFormat in availablePixelFormats {
-            actionSheet.addAction(UIAlertAction(title: pixelFormat.name, style: .default, handler: { [weak self] (action) in
-                self?.selectedPixelFormat = pixelFormat
+        actionSheet.addAction(UIAlertAction(title: "JPEG", style: .default, handler: { [weak self] (action) in
+            self?.imageFormatType = nil
             }))
-        }
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(actionSheet, animated: true, completion: nil)
     }
     
     private func startCapture(withDevice device: AVCaptureDevice, name: String) {
         capturePhotoButton.isEnabled = false
+        imageFormatType = nil
 
         if captureSession?.isRunning ?? false {
             captureSession?.stopRunning()
         }
+        captureSession = nil
         
         cameraLabel.text = name
         guard let deviceInput = try? AVCaptureDeviceInput(device: device) else {
@@ -149,7 +140,7 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         let newCaptureSession = AVCaptureSession()
         newCaptureSession.sessionPreset = AVCaptureSessionPresetPhoto
         newCaptureSession.addInput(deviceInput)
-        newCaptureSession.addOutput(captureOutput)
+        newCaptureSession.addOutput(AVCapturePhotoOutput())
         
         guard let previewLayer = AVCaptureVideoPreviewLayer(session: newCaptureSession) else {
             cameraLabel.text = "Error: AVCaptureVideoPreviewLayer"
@@ -158,11 +149,6 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         previewLayer.frame = view.bounds
         view.layer.addSublayer(previewLayer)
         
-        guard let pixelFormat = availablePixelFormats.last else {
-            pixelFormatLabel.text = "Error: Pixel Format"
-            return
-        }
-        selectedPixelFormat = pixelFormat
         
         newCaptureSession.startRunning()
         captureSession = newCaptureSession
@@ -173,53 +159,102 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         guard let captureSession = self.captureSession, captureSession.isRunning else {
             return
         }
-        
-        guard let pixelFormat = self.selectedPixelFormat else {
+        guard let captureOutput = captureSession.outputs?.first as? AVCapturePhotoOutput else {
             return
         }
         
         let photoSettings: AVCapturePhotoSettings
-        if let pixelFormat = selectedPixelFormat, pixelFormat.isRaw ?? false {
-            photoSettings = AVCapturePhotoSettings(rawPixelFormatType: pixelFormat.type)
+        if let imageFormatType = self.imageFormatType {
+            // RAW
+            photoSettings = AVCapturePhotoSettings(rawPixelFormatType: imageFormatType)
         } else {
-            let key = kCVPixelBufferPixelFormatTypeKey as String
-            photoSettings = AVCapturePhotoSettings(format: [key: NSNumber(value: pixelFormat.type)])
+            // JPEG
+            photoSettings = AVCapturePhotoSettings()
         }
+        
         captureOutput.capturePhoto(with: photoSettings, delegate: self)
     }
     
-    private func logCaptured(pixelBuffer: CVPixelBuffer) {
-        var pixelFormat = PixelFormat.pixelFormat(ofType: CVPixelBufferGetPixelFormatType(pixelBuffer))
-        pixelFormat.isRaw = false
-        let width = CVPixelBufferGetWidth(pixelBuffer)
-        let height = CVPixelBufferGetHeight(pixelBuffer)
-        let dataSize = CVPixelBufferGetDataSize(pixelBuffer)
-        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
-        let isPlanar = CVPixelBufferIsPlanar(pixelBuffer)
-        print(" -[Pixel Format]-> \(pixelFormat)")
-        print(" -[Width]-> \(width)")
-        print(" -[Height]-> \(height)")
-        print(" -[Bytes/Row]-> \(bytesPerRow)")
-        print(" -[Data Size]-> \(dataSize)")
-        print(" -[Is Planar?]-> \(isPlanar)")
-        
-        if isPlanar {
-            let planeCount = CVPixelBufferGetPlaneCount(pixelBuffer)
-            var planeWidthArray: [Int] = []
-            var planeHeightArray: [Int] = []
-            var planeBytesPerRowArray: [Int] = []
-            for planeIndex in 0..<planeCount {
-                planeWidthArray.append(CVPixelBufferGetWidthOfPlane(pixelBuffer, planeIndex))
-                planeHeightArray.append(CVPixelBufferGetHeightOfPlane(pixelBuffer, planeIndex))
-                planeBytesPerRowArray.append(CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, planeIndex))
-            }
-            print(" -[Plane Count]-> \(planeCount)")
-            print(" -[Plane Width]-> \(planeWidthArray)")
-            print(" -[Plane Height]-> \(planeHeightArray)")
-            print(" -[Plane Bytes/Row]-> \(planeBytesPerRowArray)")
+    private func saveImage(data: Data) {
+        if let image = UIImage(data: data) {
+            logImage(image: image)
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(image:didFinishSavingWithError:contextInfo:)), nil)
         }
     }
     
+    private func logImage(image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
+        print("[width] \(cgImage.width)")
+        print("[height] \(cgImage.height)")
+        print("[bytesPerRow] \(cgImage.bytesPerRow)")
+        print("[bitsPerPixel] \(cgImage.bitsPerPixel)")
+        print("[bitsPerComponent] \(cgImage.bitsPerComponent)")
+        print("[shouldInterpolate] \(cgImage.shouldInterpolate)")
+        print("[isMask] \(cgImage.isMask)")
+        
+        let alphaInfo: String
+        switch cgImage.alphaInfo {
+        case .none:
+            alphaInfo = "none"
+        case .premultipliedLast:
+            alphaInfo = "premultipliedLast"
+        case .premultipliedFirst:
+            alphaInfo = "premultipliedFirst"
+        case .last:
+            alphaInfo = "last"
+        case .first:
+            alphaInfo = "first"
+        case .noneSkipLast:
+            alphaInfo = "noneSkipLast"
+        case .noneSkipFirst:
+            alphaInfo = "noneSkipFirst"
+        case .alphaOnly:
+            alphaInfo = "alphaOnly"
+        }
+        print("[alphaInfo] \(alphaInfo)")
+        
+        let renderingIntent: String
+        switch cgImage.renderingIntent {
+        case .defaultIntent:
+            renderingIntent = "defaultIntent"
+        case .absoluteColorimetric:
+            renderingIntent = "absoluteColorimetric"
+        case .relativeColorimetric:
+            renderingIntent = "relativeColorimetric"
+        case .perceptual:
+            renderingIntent = "perceptual"
+        case .saturation:
+            renderingIntent = "saturation"
+        }
+        print("[renderingIntent] \(renderingIntent)")
+        
+        var bitmapInfo: [String] = []
+        if cgImage.bitmapInfo.contains(.alphaInfoMask) { bitmapInfo.append("alphaInfoMask") }
+        if cgImage.bitmapInfo.contains(.byteOrder16Big) { bitmapInfo.append("byteOrder16Big") }
+        if cgImage.bitmapInfo.contains(.byteOrder16Little) { bitmapInfo.append("byteOrder16Little") }
+        if cgImage.bitmapInfo.contains(.byteOrder32Big) { bitmapInfo.append("byteOrder32Big") }
+        if cgImage.bitmapInfo.contains(.byteOrder32Little) { bitmapInfo.append("byteOrder32Little") }
+        if cgImage.bitmapInfo.contains(.floatComponents) { bitmapInfo.append("floatComponents") }
+        print("[bitmapInfo] \(bitmapInfo)")
+    }
+    
+    private func showErrorAlert(_ error: Error) {
+        showAlert(title: "Error", message: error.localizedDescription)
+    }
+    
+    private func showAlert(title: String?, message: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        if let error = error {
+            showErrorAlert(error)
+        } else {
+            showAlert(title: "Saved.", message: nil)
+        }
+    }
     
     // MARK:- AVCapturePhotoCaptureDelegate
     
@@ -231,36 +266,22 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                  error: Error?) {
         print(#function)
         if let error = error {
-            print(" -[Error]-> \(error)")
+            showErrorAlert(error)
             return
         }
         
         guard let photoSampleBuffer = photoSampleBuffer else {
-            print(" -[Error]-> photoSampleBuffer is nil")
+            showAlert(title: "Error", message: "photoSampleBuffer is nil.")
             return
         }
         
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(photoSampleBuffer) else {
-            print(" -[Error]-> Cannot get image buffer from photoSampleBuffer")
-            return
-        }
-        logCaptured(pixelBuffer: pixelBuffer)
-        
-        /*
         guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(
             forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
-                print(" -[Error]-> jpegPhotoDataRepresentation is nil")
+                showAlert(title: "Error", message: "Failed to create JPEG data.")
                 return
-        }*/
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags())
-        let dataSize = CVPixelBufferGetDataSize(pixelBuffer)
-        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
-        let imageData = NSData(bytes: baseAddress, length: dataSize)
-
-        // TODO: Save to photo library by Photos.framework
-
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags())
+        }
+ 
+        saveImage(data: imageData)
     }
     
     func capture(_ captureOutput: AVCapturePhotoOutput,
@@ -271,28 +292,22 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
                  error: Error?) {
         print(#function)
         if let error = error {
-            print(" -[Error]-> \(error)")
+            showErrorAlert(error)
             return
         }
         
         guard let rawSampleBuffer = rawSampleBuffer else {
-            print(" -[Error]-> rawSampleBuffer is nil")
+            showAlert(title: "Error", message: "rawSampleBuffer is nil.")
             return
         }
 
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(rawSampleBuffer) else {
-            print(" -[Error]-> Cannot get image buffer from rawSampleBuffer")
-            return
-        }
-        logCaptured(pixelBuffer: pixelBuffer)
-        
         guard let imageData = AVCapturePhotoOutput.dngPhotoDataRepresentation(
             forRawSampleBuffer: rawSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
-                print(" -[Error]-> dngPhotoDataRepresentation is nil")
+                showAlert(title: "Error", message: "Failed to create RAW(DNG) data.")
                 return
         }
         
-        // TODO: Save to photo library by Photos.framework
+        saveImage(data: imageData)
     }
     
 
